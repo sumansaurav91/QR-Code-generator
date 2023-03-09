8394e5b66a11460b7532cd9505e73d3e1faa0c93
Saurav, Suman <SumanSaurav@rocketmortgage.com>
2023-03-09 00:08:18 -0500

    QR Code

---
+++ Farmonks.Generator/Controllers/QrController.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+
+namespace Farmonks.Generator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QrController : Controller
+    {
+        [HttpGet]
+        [Route("qenerate/{qrText}")]
+        public IActionResult GetQrCode(string qrText)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            return File(BitmapToBytes(qrCodeImage), "image/jpeg");
+        }
+
+        [HttpGet]
+        [Route("qenerate-color/{qrText}")]
+        public IActionResult GetColorQrCode(string qrText)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.DarkRed, Color.PaleGreen, true);
+
+            return File(BitmapToBytes(qrCodeImage), "image/jpeg");
+        }
+
+        [HttpGet]
+        [Route("qenerate-logo/{qrText}")]
+        public IActionResult GetLogoQrCode(string qrText)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(qrText, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, (Bitmap)Bitmap.FromFile("Images\\farmonk.png"));
+
+            return File(BitmapToBytes(qrCodeImage), "image/jpeg");
+        }
+
+        private static Byte[] BitmapToBytes(Bitmap img)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+    }
+}
